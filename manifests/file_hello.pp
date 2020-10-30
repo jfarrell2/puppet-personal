918692ed94efd2e04863260ad626f327f39b7094
Joseph Farrell <jfarrell2@gannett.com>
2020-10-30 13:35:43 -0500

    Adding directly to Master

--- manifests/file_hello.pp
+++ manifests/file_hello.pp
@@ -1,6 +1,7 @@
 file { '/tmp/hello.txt' 
    ensure => file,
    content => "Hello, World\n",
-   content => "text added via branch\n"
+   content => "text added via branch\n",
+   content => "Added directly in Master Branch\n",
 }
 
